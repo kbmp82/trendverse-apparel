1323b5caa5266de82ac63b0adb503ac2c5061136
Kevin Brown <kbmp82@gmail.com>
2024-01-16 12:36:16 -0700

    add links to homepage categories

--- src/components/Categories/CategoryCard.js
+++ src/components/Categories/CategoryCard.js
@@ -5,14 +5,19 @@ import {
   CategoryImage,
   Category,
 } from "./categories.styles";
+import { useNavigate } from "react-router-dom";
+
 export default function Card({ item }) {
+  const navigate = useNavigate();
   return (
     <Category className={"category--" + item.size}>
       <div className="category__wrapper">
         <CategoryImage src={item.image} alt={item.title} />
         <CategoryContent>
           <p className="category__title">{item.title}</p>
-          <CategoryCTA>shop now</CategoryCTA>
+          <CategoryCTA onClick={() => navigate(item.route)}>
+            shop now
+          </CategoryCTA>
         </CategoryContent>
       </div>
     </Category>
--- src/routes/Home/Homepage.js
+++ src/routes/Home/Homepage.js
@@ -8,26 +8,31 @@ export default function Homepage() {
       title: "Hats",
       size: "medium",
       image: "https://i.ibb.co/cvpntL1/hats.png",
+      route: "shop/hats",
     },
     {
       title: "Jackets",
       size: "medium",
       image: "https://i.ibb.co/px2tCc3/jackets.png",
+      route: "shop/jackets",
     },
     {
       title: "Sneakers",
       size: "medium",
       image: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      route: "shop/sneakers",
     },
     {
       title: "Womens",
       size: "large",
       image: "https://i.ibb.co/GCCdy8t/womens.png",
+      route: "shop/womens",
     },
     {
       title: "Mens",
       size: "large",
       image: "https://i.ibb.co/R70vBrQ/men.png",
+      route: "shop/mens",
     },
   ];
   return (
